b551bd5084573132602f1abcdbb727915b3b3edf
shantista <shantdani@gmail.com>
2013-07-19 10:07:51 -0700

    Display method added

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,20 @@
 
 public class Driver {
 	
-	public static void main(String[] args){
 	
-		System.out.println("Hello world");
+	
+	public static void main(String[] args){
+		new Driver();
+		
 	}
 	
+	public Driver(){
+		System.out.println("Hello world");
+		Display("Lol");
+		
+	}
+	
+	public void Display(String sentence){
+		System.out.println(sentence);
+	}
 }
